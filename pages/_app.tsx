dcc7592e92d6f35083a27316b7fd9c92154b3cbe
Alex <avseenkov_a@mail.ru>
2021-03-06 11:26:02 +0100

    add material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from 'src/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
 
 export default MyApp;
